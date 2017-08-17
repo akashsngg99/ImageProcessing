03a660d5a8c057fb0e02e0b8df5fad6c47f0c1a7
Junhui Zhang <mrlittlezhu@gmail.com>
2017-08-17 16:07:06 +0800

    Delete Python Script.py

--- .idea/fileTemplates/internal/Python Script.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : ${DATE} ${TIME}
-# @Author  : mrlittlepig
-# @Site    : www.mrlittlepig.xyz
-# @File    : ${NAME}.py
-# @Software: ${PRODUCT_NAME}
